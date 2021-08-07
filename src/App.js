27b0798751fb567ac7912c0640d7244bec9601e0
pullahjef <pullahjeffrey@gmail.com>
2021-08-07 12:52:16 +0100

    ReactNew

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import Hero from './Hero';
+import './App.css';
+
+const App() {
+  const data = [
+    {
+      title: "Html",
+      description: "This is Html"
+    },
+    {
+      title: "Css",
+      description: "This is CSS"
+    }
+    {
+      title: "Javasript",
+      description: "This is Javascript"
+    }
+    {
+      title: "React",
+      description: "This is React"
+    }
+    {
+      title: "Vue",
+      description: "This is Vue"
+    }
+    {
+      title: "Angular",
+      description: "This is Angular"
+    }
+  ]
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
