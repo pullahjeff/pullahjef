a773942c65a456f2b13a9f996bd536503e40609a
pullahjef <pullahjeffrey@gmail.com>
2021-08-07 13:31:51 +0100

    ReactNew

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
-import Hero from './Hero';
-import './App.css';
+import './App.css'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import React from "react";
+//import Home from './pages/Home';
+//import About from './pages/About';
+//import Contact from './pages/Contact';
+//import Forms from './Forms'
 
-const App() {
-  const data = [
-    {
-      title: "Html",
-      description: "This is Html"
-    },
-    {
-      title: "Css",
-      description: "This is CSS"
-    }
-    {
-      title: "Javasript",
-      description: "This is Javascript"
-    }
-    {
-      title: "React",
-      description: "This is React"
-    }
-    {
-      title: "Vue",
-      description: "This is Vue"
-    }
-    {
-      title: "Angular",
-      description: "This is Angular"
-    }
-  ]
+import {Switch, Route, Link} from "react-router-dom";
+
+function App() {
   return (
-    <div className="App">
-      Hello World
+
+    <div>
+     <nav>
+       <Link to="/About">About Page</Link>
+      <Link to="/Contact">Contact Page</Link>
+      <Link to="/Login">Log In Page</Link>
+      <Link to="/Home">Home Page</Link>
+      
+      </nav>
+   
+     
+
+     <Switch>
+     <Route path="/About">
+       <About/>
+       </Route>
+       <Route path="/Login">
+       <Login/>
+     </Route>
+     <Route path="/Contact">
+       <Contact/>
+     </Route>
+     <Route path="/Home">
+       <Home/>
+     </Route>
+     </Switch>
+
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
